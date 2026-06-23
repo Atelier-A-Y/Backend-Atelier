ec94a5fa8becadeab2d98d488de955514195eb46
agathahelena <agatha.helena.so@gmail.com>
2026-06-23 08:35:38 -0300

    Feat : adicionar tamanho em roupa

--- core/models/venda.py
+++ core/models/venda.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Venda(models.Model):
     preco_total = models.DecimalField(max_digits=10, decimal_places=2)
+    data_venda = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'Venda {self.id}'
