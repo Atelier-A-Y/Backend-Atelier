84e3f6d1a96fd7beb5de8e3332d257a988701337
agathahelena <agatha.helena.so@gmail.com>
2026-06-26 14:38:05 -0300

    feat: organização do código #12

--- core/models/estoque.py
+++ core/models/estoque.py
@@ -2,11 +2,7 @@ from django.db import models
 
 
 class Estoque(models.Model):
-    roupa = models.OneToOneField(
-        'Roupa',
-        on_delete=models.CASCADE,
-        related_name='estoque'
-    )
+    roupa = models.OneToOneField('Roupa', on_delete=models.CASCADE, related_name='estoque')
     quantidade = models.PositiveIntegerField(default=0)
 
     def __str__(self):
@@ -19,34 +15,12 @@ class MovimentacaoEstoque(models.Model):
         ENTRADA = 'E', 'Entrada'
         SAIDA = 'S', 'Saída'
 
-    roupa = models.ForeignKey(
-        'Roupa',
-        on_delete=models.CASCADE,
-        related_name='movimentacoes'
-    )
-
-    tipo = models.CharField(
-        max_length=1,
-        choices=Tipo.choices
-    )
-
+    roupa = models.ForeignKey('Roupa', on_delete=models.CASCADE, related_name='movimentacoes')
+    tipo = models.CharField(max_length=1, choices=Tipo.choices)
     quantidade = models.PositiveIntegerField()
-
     data = models.DateTimeField(auto_now_add=True)
-
-    compra = models.ForeignKey(
-        'Compra',
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True
-    )
-
-    venda = models.ForeignKey(
-        'Venda',
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True
-    )
+    compra = models.ForeignKey('Compra', on_delete=models.SET_NULL, null=True, blank=True)
+    venda = models.ForeignKey('Venda', on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return f'{self.get_tipo_display()} - {self.roupa.nome} ({self.quantidade})'
