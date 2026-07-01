9aaf43270974e2cc591edfde8c3bf8d25d6cd071
AndressaSteffen26 <andressasteffen.ifc.info3@gmail.com>
2026-07-01 11:14:54 -0300

    feat: alteração da model e do serializer de favoritos #10

--- core/admin.py
+++ core/admin.py
@@ -6,17 +6,25 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
-from core import models
+from core.models import (
+    Categoria,
+    Compra,
+    Favorito,
+    Roupa,
+    User,
+)
 
 
+@admin.register(User)
 class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
 
     ordering = ['id']
     list_display = ['email', 'name']
+    search_fields = ['email', 'name', 'groups__name']
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        (_('Personal Info'), {'fields': ('name',)}),
+        (_('Personal Info'), {'fields': ('name', 'foto', 'tipo_usuario')}),
         (
             _('Permissions'),
             {
@@ -42,8 +50,6 @@ class UserAdmin(BaseUserAdmin):
                     'password1',
                     'password2',
                     'name',
-                    'telefone',
-                    'cpf',
                     'is_active',
                     'is_staff',
                     'is_superuser',
@@ -53,11 +59,37 @@ class UserAdmin(BaseUserAdmin):
     )
 
 
-admin.site.register(models.User, UserAdmin)
-admin.site.register(models.Roupa)
-admin.site.register(models.Continente)
-admin.site.register(models.Categoria)
-admin.site.register(models.Compra)
-admin.site.register(models.Venda)
-admin.site.register(models.Tamanho)
-admin.site.register(models.TipoPagamento)
+@admin.register(Roupa)
+class RoupaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'preco', 'categoria')
+    search_fields = ('nome', 'preco', 'categoria__descricao')
+    list_filter = ('categoria',)
+    ordering = ('nome', 'preco', 'categoria')
+
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('descricao',)
+    search_fields = ('descricao',)
+    list_filter = ('descricao',)
+    ordering = ('descricao',)
+
+
+@admin.register(Compra)
+class CompraAdmin(admin.ModelAdmin):
+    list_display = ('id', 'usuario', 'status', 'total')
+    readonly_fields = ('data',)
+    search_fields = ('usuario', 'status')
+    list_filter = ('usuario', 'status')
+    ordering = ('status', 'usuario', 'data', 'total')
+    list_per_page = 25
+
+
+@admin.register(Favorito)
+class FavoritoAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'livro', 'nota', 'data_atualizacao')
+    search_fields = ('usuario__email', 'livro__titulo', 'comentario')
+    list_filter = ('nota', 'data_atualizacao')
+    ordering = ('-data_atualizacao',)
+    list_per_page = 25
+    readonly_fields = ('data_atualizacao',)
--- core/models/favorito.py
+++ core/models/favorito.py
@@ -1,12 +1,23 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+
+from .roupa import Roupa
+from .user import User
 
 
 class Favorito(models.Model):
-    roupa = models.ForeignKey('Roupa', on_delete=models.CASCADE, related_name='favoritos')
-    usuario = models.ForeignKey('User', on_delete=models.CASCADE, related_name='favoritos')
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, related_name='favoritos')
+    roupa = models.ForeignKey(Roupa, on_delete=models.CASCADE, related_name='favoritos')
+    data_atualizacao = models.DateTimeField(auto_now=True)
+    nota = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], null=True, blank=True)
+    comentario = models.TextField(null=True, blank=True)
+
+    class Meta:
+        ordering = ['-data_atualizacao']
+        unique_together = ['usuario', 'roupa']  # Impede duplicatas de favoritos
 
     def __str__(self):
-        return f'Favorito: {self.roupa.id} - {self.usuario.id}'
+        return f'{self.usuario} - {self.roupa}'
 
     class Meta:
         verbose_name_plural = 'Favoritos'
