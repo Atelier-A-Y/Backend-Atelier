e8d2efe3568f7ce145ea70278cc22b12dfa7fb01
agathahelena <agatha.helena.so@gmail.com>
2026-07-01 10:59:47 -0300

    Feat : adicionei ItemVendas e constraints no carrinho

--- core/models/carrinho.py
+++ core/models/carrinho.py
@@ -2,12 +2,14 @@ from django.db import models
 
 
 class Carrinho(models.Model):
-    quantidade = models.PositiveIntegerField(default=0)
-    roupa = models.ForeignKey('Roupa', on_delete=models.CASCADE, related_name='carrinhos')
     usuario = models.ForeignKey('User', on_delete=models.CASCADE, related_name='carrinhos')
+    roupa = models.ForeignKey('Roupa', on_delete=models.CASCADE, related_name='carrinhos')
+    quantidade = models.PositiveIntegerField(default=1)
 
     def __str__(self):
-        return str(self.quantidade)
+        return f'{self.usuario} - {self.roupa} ({self.quantidade})'
 
     class Meta:
         verbose_name_plural = 'Carrinhos'
+
+        constraints = [models.UniqueConstraint(fields=['usuario', 'roupa'], name='unique_roupa_usuario')]
--- core/models/venda.py
+++ core/models/venda.py
@@ -4,9 +4,20 @@ from .tipoPagamento import TipoPagamento
 
 
 class Venda(models.Model):
+    usuario = models.ForeignKey('User', on_delete=models.PROTECT, related_name='vendas', null=True, blank=True)
     preco_total = models.DecimalField(max_digits=10, decimal_places=2)
     data_venda = models.DateTimeField(auto_now_add=True)
-    tipoPagamento = models.ForeignKey(TipoPagamento, on_delete=models.SET_NULL, blank=True, null=True)
+    tipo_pagamento = models.ForeignKey(TipoPagamento, on_delete=models.SET_NULL, null=True, blank=True, related_name='vendas')
 
     def __str__(self):
-        return f'Venda {self.id}'
+        return f'Venda #{self.id}'
+
+
+class ItemVenda(models.Model):
+    venda = models.ForeignKey(Venda, on_delete=models.CASCADE, related_name='itens')
+    roupa = models.ForeignKey('Roupa', on_delete=models.PROTECT, related_name='itens_venda')
+    quantidade = models.PositiveIntegerField()
+    preco_unitario = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.quantidade}x {self.roupa.nome}'
