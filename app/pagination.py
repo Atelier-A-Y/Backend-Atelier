8ef30085b1858a3c0f8e185c8941fcd032b7366b
yuriramos223 <gabriela.heloisa.ramos.ifc@gmail.com>
2026-07-03 14:57:16 -0300

    fix: alteração no número de produtos permitido por página

--- app/pagination.py
+++ app/pagination.py
@@ -3,9 +3,9 @@ from rest_framework.response import Response
 
 
 class CustomPagination(pagination.PageNumberPagination):
-    page_size = 10  # tamanho padrão da página
+    page_size = 100000  # tamanho padrão da página
     page_size_query_param = 'page_size'  # permitir mudar via query parameter
-    max_page_size = 100  # tamanho máximo da página que pode ser solicitado
+    max_page_size = 100000  # tamanho máximo da página que pode ser solicitado
 
     def get_paginated_response(self, data):
         return Response({
