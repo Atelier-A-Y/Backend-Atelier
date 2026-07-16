05a80db6fce6e26276e2d9b0ee739ad683bf33e8
yuriramos223 <gabriela.heloisa.ramos.ifc@gmail.com>
2026-07-15 21:20:58 -0300

    Feat: ajustes no carrinho e nas rotas

--- core/views/roupa.py
+++ core/views/roupa.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from core.models import Carrinho, ItemCarrinho, Roupa
+from core.models import Carrinho, Roupa
 from core.serializers.roupa import (
     RoupaCreateSerializer,
     RoupaRetrieveSerializer,
@@ -23,23 +23,16 @@ class RoupaViewSet(ModelViewSet):
 
         return RoupaSerializer
 
-    @action(detail=True, methods=["post"], permission_classes=[IsAuthenticated])
-    def adicionar_carrinho(self, request, pk=None):
+@action(detail=True, methods=["post"], permission_classes=[IsAuthenticated])
+def adicionar_carrinho(self, request, pk=None):
 
-        carrinho, _ = Carrinho.objects.get_or_create(
-            usuario=request.user
-        )
+    roupa = self.get_object()
 
-        roupa = self.get_object()
+    carrinho, _ = Carrinho.objects.get_or_create(
+        usuario=request.user
+    )
 
-        item, criado = ItemCarrinho.objects.get_or_create(
-            carrinho=carrinho,
-            roupa=roupa,
-        )
+    carrinho.roupa = roupa
+    carrinho.save()
 
-        if not criado:
-            item.quantidade += request.data.get("quantidade", 1)
-
-        item.save()
-
-        return Response({"mensagem": "Produto adicionado"})
+    return Response({"mensagem": "Produto adicionado"})
