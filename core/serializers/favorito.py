8b4f02af69d274287d177f147ed122c577991c21
yuriramos223 <gabriela.heloisa.ramos.ifc@gmail.com>
2026-08-18 08:53:16 -0300

    Feat: ajustes na página favoritos

--- core/serializers/favorito.py
+++ core/serializers/favorito.py
@@ -1,10 +1,11 @@
+from core.models.roupa import Roupa
 from rest_framework.serializers import ModelSerializer
-
+from core.serializers import RoupaSerializer
 from core.models import Favorito
 
 
 class FavoritoSerializer(ModelSerializer):
+    roupa = RoupaSerializer(read_only=True)
     class Meta:
         model = Favorito
         fields = '__all__'
-        read_only_fields = ['usuario']
