61d7192d94195343f54985b78c24ea0e6c31793d
yuriramos223 <gabriela.heloisa.ramos.ifc@gmail.com>
2026-08-18 16:08:59 -0300

    Feat: ajustes no favoritoserializer

--- core/serializers/favorito.py
+++ core/serializers/favorito.py
@@ -1,11 +1,27 @@
-from core.models.roupa import Roupa
-from rest_framework.serializers import ModelSerializer
-from core.serializers import RoupaSerializer
-from core.models import Favorito
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
+
+from core.models import Favorito, Roupa
+from .roupa_serializers import RoupaRetrieveSerializer
 
 
 class FavoritoSerializer(ModelSerializer):
-    roupa = RoupaSerializer(read_only=True)
+
+    roupa = RoupaRetrieveSerializer(read_only=True)
+
+    roupa_id = PrimaryKeyRelatedField(
+        source='roupa',
+        queryset=Roupa.objects.all(),
+        write_only=True
+    )
+
     class Meta:
         model = Favorito
-        fields = '__all__'
+        fields = [
+            'id',
+            'roupa',
+            'roupa_id',
+            'usuario',
+            'data_criacao',
+            'data_atualizacao',
+        ]
+        read_only_fields = ['usuario']
