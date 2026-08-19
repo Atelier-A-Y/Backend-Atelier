a0c8431d24e94916b86bd299db29c55755b0155c
yuriramos223 <gabriela.heloisa.ramos.ifc@gmail.com>
2026-08-19 10:41:42 -0300

    Feat: ajustes no coração de favoritos

--- core/serializers/favorito.py
+++ core/serializers/favorito.py
@@ -1,10 +1,22 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
 from core.serializers.roupa import RoupaRetrieveSerializer
-from core.models import Favorito
+from core.models import Favorito, Roupa
 
 
 class FavoritoSerializer(ModelSerializer):
-    roupa = RoupaRetrieveSerializer(read_only=True)
+    roupa = PrimaryKeyRelatedField(
+        queryset=Roupa.objects.all()
+    )
+
     class Meta:
         model = Favorito
-        fields = '__all__'
+        fields = "__all__"
+        read_only_fields = ["usuario"]
+
+
+class FavoritoRetrieveSerializer(ModelSerializer):
+    roupa = RoupaRetrieveSerializer(read_only=True)
+
+    class Meta:
+        model = Favorito
+        fields = "__all__"
--- core/views/favorito.py
+++ core/views/favorito.py
@@ -3,14 +3,24 @@ from rest_framework.viewsets import ModelViewSet
 
 from core.models import Favorito
 from core.serializers import FavoritoSerializer
+from core.serializers.favorito import FavoritoRetrieveSerializer
 
 
 class FavoritoViewSet(ModelViewSet):
-    serializer_class = FavoritoSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        return Favorito.objects.filter(usuario=self.request.user)
+        return Favorito.objects.filter(
+            usuario=self.request.user
+        )
+
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return FavoritoRetrieveSerializer
+
+        return FavoritoSerializer
 
     def perform_create(self, serializer):
-        serializer.save(usuario=self.request.user)
+        serializer.save(
+            usuario=self.request.user
+        )
